33f42f606a7a2f44c227b213e5bc2cf77c61487a
Mao-Lin Li <molimomo0@gmail.com>
2015-07-08 12:33:25 -0500

    Updated correct quick sort code.

--- quickSort.cpp
+++ quickSort.cpp
@@ -3,7 +3,45 @@
 
 using namespace std;
 
-int main(){
+void quickHelp(int *arr, int begin, int end){
+	int left = begin;
+	int right = end;
+	int pivot = arr[begin];
+	
+	while(left<=right){
+		while(arr[left] < pivot) left++;
+		while(arr[right] > pivot) right--;
+		if(left<=right){
+			swap(arr[left],arr[right]);
+			left++;
+			right--;
+		}	
+	}
+	
+	if(begin < right)
+		quickHelp(arr, begin, right);
+	if(left < end)
+		quickHelp(arr, left, end);
+}
 
+
+void quickSort(int *arr){
+	cout<<"Before: "<<endl;
+	for(int i=0;i<10;i++){
+		cout<<arr[i]<<", ";
+	}
+	cout<<endl;
+	quickHelp(arr, 0, 9);
+}
+
+
+int main(){
+	int test[10] = {8,3,25,6,10,17,1,2,18,5};
+	quickSort(test);
+	cout<<"After: "<<endl;
+	for(int i=0;i<10;i++){
+		cout<<test[i]<<", ";
+	}
+	cout<<endl; 
 	return 0;
 }
