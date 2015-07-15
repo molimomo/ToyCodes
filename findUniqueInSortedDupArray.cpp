53501d3dd116ca44a42663cad9a4e9bb49ab7bae
Mao-Lin Li <molimomo0@gmail.com>
2015-07-15 13:42:40 -0500

    Modfied program that adding another implementation with binary search. function name: funBetter. (more efficient).

--- findUniqueInSortedDupArray.cpp
+++ findUniqueInSortedDupArray.cpp
@@ -30,8 +30,40 @@ int fun(int *arr, int begin, int end){
     }
 }
 
+int funBetter(int* arr, int begin, int end){
+    if(begin>end)
+        return -1;
+
+    if(begin==end)
+        return begin;
+
+    int mid  = (begin+end)/2;
+
+    if(mid%2==0){
+        // target is before mid
+        if(arr[mid+1]!=arr[mid]){
+            return funBetter(arr, begin, mid);
+        }
+        // target is after mid
+        else{
+            return funBetter(arr, mid+2, end);
+        }
+    }
+    else{
+        // target is before mid
+        if(arr[mid-1]==arr[mid]){
+            return funBetter(arr, mid+2,end);
+        }
+        else{
+            return funBetter(arr, begin, mid-1);
+        }
+    }
+}
+
+
 int main(){
-    int arr[] = {1, 1, 3, 3, 4, 5, 5, 7, 7, 8, 8};
+    int arr[] = {1, 3, 3, 4, 4,5, 5, 7, 7, 8, 8};
     cout<<arr[fun(arr, 0, (sizeof(arr)/sizeof(arr[0]))-1)]<<endl;    
+    cout<<arr[funBetter(arr, 0, (sizeof(arr)/sizeof(arr[0]))-1)]<<endl;    
     return 0;
 }
